e0141dee8a4b8c837bb413626ff81d3adbc7d18a
joel-furrer <joel.furrer426@gmail.com>
2025-05-26 17:58:30 +0200

    removed home image

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,8 @@
-import wallpaper from "@Assets/bee-wallaper.jpg"
 import "./Home.css"
 
 function Home() {
     return (
         <div className="container__home" >
-            <img
-                className="home__wallpaper"
-                src={wallpaper}
-                alt="wallpaper"
-            />
         </div>
     )
 }
