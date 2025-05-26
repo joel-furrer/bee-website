a9ed5f5fffc9f8f6b2cf5c81f76f34b282e07d71
joel-furrer <joel.furrer426@gmail.com>
2025-05-26 19:28:13 +0200

    added footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,18 @@
-
+import "./Footer.css"
 
 function Footer() {
     return (
-        <footer>
-            <p>Footer</p>
+        <footer className="bee-footer">
+            <div className="bee-footer-content">
+                <p>&copy; {new Date().getFullYear()} Abeille – Eine Seite über Bienen</p>
+                <nav>
+                    <a href="/wiki">Wiki</a>
+                    <a href="/honey">Honig</a>
+                    <a href="/tasks">Aufgaben</a>
+                </nav>
+            </div>
         </footer>
     )
 }
 
-export default Footer;
+export default Footer
